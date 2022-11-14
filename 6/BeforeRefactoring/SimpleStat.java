564a4e029f604688b04c6f70dfaaaeadb5b46084
YeoJiSu <duwltn1301@naver.com>
2022-11-14 09:21:45 +0900

    원래 파일

--- 6/BeforeRefactoring/SimpleStat.java
+++ 6/BeforeRefactoring/SimpleStat.java
@@ -2,32 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class SimpleCalculator {
+public class SimpleStat {
 	public static void main(String[] args) {
-		printInputMessage();
-		List<Integer> numArray = getNumArray();
-		
-		closeInputMessage();
-
-		long sum = getSum(numArray );
-		long product = getProduct(numArray );
-
-		printSum(sum);
-		printProduct(product);
-	}
-	
-	
-}
-
-public class NumArrayMethod{
-	private void printInputMessage(){
 		Scanner s1 = new Scanner(System.in);
 		System.out.println("Enter integer ( 0 or minus for Exit)");
-	}
-	private void closeInputMessage(){
-		s1.close();
-	}
-	private List<Integer> getNumArray(){
 		List<Integer> ns = new ArrayList<>();
 		while (true) {
 			int n = s1.nextInt();
@@ -35,31 +13,14 @@ public class NumArrayMethod{
 				break;
 			ns.add(n);
 		}
-		return ns;
-	}
-	
-}
-public class SumMethod {
-	long sum = 0;
-	private long getSum(List numArray){
-		for (int element : numArray) {
-			sum += element;
+		s1.close();
+		long s = 0;
+		long p = 1;
+		for (int i : ns) {
+			s += i;
+			p *= i;
 		}
-		return sum;
-	}
-	private void printSum(long sum){
-			System.out.println("Sum: " + sum);
-	}
-}
-public class ProductMethod {
-	long product = 1;
-	private long getProduct(List numArray){
-		for (int element : numArray) {
-			product *= element;
-		}
-		return product;
-	}
-	private void printProduct(long product){
-		System.out.println("Product: " + product);
+		System.out.println("Sum: " + s);
+		System.out.println("Product: " + p);
 	}
 }
